0f20544b889c4bc88a425d2be9b6d65446f1050d
Casey Burnett <burnett.casey@gmail.com>
2016-05-25 13:28:33 -0600

    final submission

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-alert('passed');
+// Problem 1
+// var x = 0;
+// while (x <= 500){
+// 	console.log(x);
+// 	x = x + 100;
+// }
+
+// Problem 2
+var y = 1;
+while (y <= 64){
+	console.log(y);
+	y = y * 2;
+}
+
+// Problem 3 
+// var z = 1;
+// while (z <=3){
+// 	i = 0;
+// 	while (i < 3){
+// 		console.log(z);
+// 		i++;
+// 	}
+// 	if (z === 3){
+// 		break
+// 	}else {
+// 		z++
+// 	}
+	
+// }
+
+// Problem 4
+// var aa = 0;
+// while ( aa <= 10 ){
+// 	console.log(aa)
+// 	aa += 2
+// }
+	
+// Problem 5
+// var bb = 0;
+// while ( bb < 15){
+// 	bb += 3;
+// 	console.log(bb);
+	
+// }
+
+// Problem 6 
+// var cc = 10;
+// while ( cc > 0 ){
+// 	cc --
+// 	console.log( cc );
+
+// }
+
+// Problem 7 
+// var dd = 0;
+// while (dd < 3){
+// 	dd++
+// 	report = 0
+// 	while (report < 4){
+// 		console.log(report);
+// 		report ++;
+// 	}
+// }
